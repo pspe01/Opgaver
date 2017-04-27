a95d964551082ea66d1db44f9b242ec1c7d650ec
Palle Pedersen <pspe01@gmail.com>
2017-04-27 15:10:55 +0200

    tt

---
+++ Opg01HelloWorld1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg01HelloWorld1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indtast navn: ");
+            string navn = Console.ReadLine();
+            Console.WriteLine("Velkommen " + navn.ToUpper());
+            Console.ReadKey();
+            Console.WriteLine();
+
+        }
+    }
+}
