5837116bcf3a82fc911816eb633f533226132b3c
Palle Pedersen <pspe01@gmail.com>
2017-04-28 14:42:51 +0200

    Opgave med string

---
+++ Opg04String/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg04String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Palle";
+            string efternavn = "Pedersen";
+            string samletNavn;
+            string navnStort;
+            string navnLille;
+            string navndel;
+
+
+            samletNavn = fornavn + " " + efternavn;
+            navnStort = samletNavn.ToUpper();
+            navnLille = samletNavn.ToLower();
+            navndel = samletNavn.Substring(6, 4);
+            string[] navnsplit = samletNavn.Split(' ');
+
+            Console.WriteLine(samletNavn);
+            Console.WriteLine(navnStort);
+            Console.WriteLine(navnLille);
+            
+
+            foreach (var item in navnsplit)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
