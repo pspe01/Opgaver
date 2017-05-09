437616834a1107834a870829a0c57d26ea17bf61
Palle Pedersen <pspe01@gmail.com>
2017-05-09 10:38:34 +0200

    opg08

---
+++ Opg08Egenskaber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg08Egenskaber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vare v1 = new Vare("Telefon", 2000);
+
+            Console.WriteLine(v1.Navn);
+            Console.WriteLine(v1.Pris);
+
+            Double pMoms = v1.PrisMedMoms();
+            Console.WriteLine(pMoms);
+        }
+    }
+}
---
+++ Opg08Egenskaber/Vare.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg08Egenskaber
+{
+    public class Vare
+    {
+
+        public Vare()
+        {
+
+        }
+
+        public Vare(string navn, int pris)
+        {
+            this.Navn = navn;
+            this.Pris = pris;
+        }
+
+        private string navn;
+
+        public string Navn
+        {
+            get
+            {
+                Console.WriteLine("------Navn hentes i klasse------------");
+                return navn;
+            }
+            set
+            {
+                Console.WriteLine("------Navn tildeles værdi i klassen------------");
+                navn = value;
+            }
+        }
+
+        private Double pris;
+
+        public Double Pris
+        {
+            get
+            {
+                Console.WriteLine("------Pris hentes i klasse--------------");
+                return pris;
+            }
+            set
+            {
+                Console.WriteLine("------Pris tildeles værdi i klassen------------");
+                pris = value;
+            }
+        }
+
+
+        public double PrisMedMoms()
+        {
+            Console.WriteLine("------Pris med moms fra metode------------");
+            return this.pris = this.pris * 1.25;
+        }
+
+
+    }
+}
