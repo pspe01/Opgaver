8130abbeffdda663a90c620feb8b85f862bbd8ac
PSP <pspe01@gmail.com>
2017-05-30 15:47:35 +0200

    .

---
+++ Opg15Delegate/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg15Delegate
+{
+    class Program
+    {
+
+        public delegate int MyDelegateBeregn(int a,int b);
+
+        static void Main(string[] args)
+        {
+            int res;
+
+            res = Beregner(5, 5, Plus);
+            Console.WriteLine(res);
+            Console.WriteLine("");
+
+            res = Beregner(10,5, Minus);
+            Console.WriteLine(res);
+            Console.WriteLine("");
+
+            res = Beregner(10, 5, Divider);
+            Console.WriteLine(res);
+            Console.WriteLine("");
+
+            res = Beregner(10000, 20, Potens);
+            Console.WriteLine(res);
+            Console.WriteLine("");
+
+
+        }
+
+        public static int Beregner(int a, int b, MyDelegateBeregn r)
+        {
+            return r(a, b);
+        }
+
+        public static int Plus(int a, int b)
+
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+
+        {
+            return a - b;
+        }
+
+        public static int Divider(int a, int b)
+
+        {
+            return a / b;
+        }
+
+        public static int Gange(int a, int b)
+
+        {
+            return a * b;
+        }
+
+        public static int Potens(int a, int b)
+
+        {
+            double aa = Convert.ToDouble(a);
+            double bb = Convert.ToDouble(b);
+
+
+            try
+            {
+                return Convert.ToInt32(Math.Pow(aa, bb));
+            }
+            catch (Exception ex)
+            {
+
+                ApplicationException ex1 = new ApplicationException("Der er sket en fejl: Første int " + aa + " anden int: " + bb, ex);
+                throw ex1;
+            }
+            
+        }
+    }
+}
---
+++ Opg16Watch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg16Watch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.FileSystemWatcher watcher = new System.IO.FileSystemWatcher();
+
+            watcher.Path = @"c:\temp";
+            watcher.EnableRaisingEvents = true;
+          //  watcher.NotifyFilter = System.IO.NotifyFilters.LastWrite;
+            watcher.Changed += Watcher_Changed;
+
+            do
+            {
+
+            } while (true);
+        }
+
+        private static void Watcher_Changed(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine("Der er sket ændringer " + e.Name);
+        }
+    }
+}
